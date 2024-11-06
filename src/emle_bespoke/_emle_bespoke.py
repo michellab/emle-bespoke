fc58eabb85116f4546f21eee8a02b07e7ff6fff9
Joao Morado <joaomorado@gmail.com>
2024-11-06 17:53:52 +0000

    Update EMLEBespoke

--- src/emle_bespoke/_emle_bespoke.py
+++ src/emle_bespoke/_emle_bespoke.py
@@ -1,14 +1,25 @@
-# from emle.train import EMLETrainer as _EMLETrainer
+import logging as _logging
+
 import torch as _torch
 from emle.models import EMLEBase as _EMLEBase
 from emle.train import EMLETrainer as _EMLETrainer
 
+logger = _logging.getLogger(__name__)
 
 class EMLEBespoke:
-    """Class to train bespoke EMLE models."""
+    """
+    Class to train bespoke EMLE models.
 
-    def __init__(self, ref_calculator, emle_base: _EMLEBase = None):
-        self._ref_calculator = ref_calculator
+    Parameters
+    ----------
+    ref_sampler : ReferenceDataSampler
+        The reference data sampler.
+    emle_base : EMLEBase, optional
+        The EMLE base model to use. Default is EMLEBase.
+    """
+
+    def __init__(self, ref_sampler, emle_base: _EMLEBase = _EMLEBase):
+        self._ref_sampler = ref_sampler
         self._emle_base = emle_base
 
     def train_model(
@@ -19,26 +30,62 @@ class EMLEBespoke:
         alpha_mode="species",
         sigma=1e-3,
         ivm_thr=0.02,
-        epochs=10000,
+        epochs=100,
         lr_qeq=0.01,
         lr_thole=0.01,
         lr_sqrtk=0.01,
-        model_filename="mymodel.mat",
+        model_filename="bespoke_model.mat",
         device=_torch.device("cuda"),
         dtype=_torch.float64,
     ):
-        # Sample reference data
-        for i in n_samples:
-            self._ref_calculator.sample(n_steps)
+        """
+        Sample reference data and train a bespoke model.
 
-        # Train bespoke model
+        Parameters
+        ----------
+        n_samples : int
+            The number of samples to generate.
+        n_steps : int
+            The number of steps between each sample.
+        train_mask : torch.Tensor, optional
+            The training mask. Default is None, which means all data is used.
+        alpha_mode : str, optional
+            The alpha mode.
+        sigma : float, optional
+            The sigma value.
+        ivm_thr : float, optional
+            The ivm threshold.
+        epochs : int, optional
+            The number of epochs.
+        lr_qeq : float, optional
+            The learning rate for qeq.
+        lr_thole : float, optional
+            The learning rate for thole.
+        lr_sqrtk : float, optional
+            The learning rate for sqrtk.
+        model_filename : str, optional
+            The model filename.
+        device : torch.device, optional
+            The device. Default is cuda.
+        dtype : torch.dtype, optional
+            The data type. Default is float64.
+        """
+        logger.info("Starting bespoke model training.")
+        logger.info(f"Number of samples to sample: {n_samples}")
+        logger.info(f"Number of steps per sample: {n_steps}")
+        for i in range(n_samples):
+            ref_data = self._ref_sampler.sample(n_steps)
+            logger.info(f"Sampled {i + 1}/{n_samples} configurations.")
+        logger.info("Finished sampling reference data.")
+
+        logger.info("Training bespoke model.")
         self._train(
-            z=self._ref_calculator.ref_data["z"],
-            xyz=self._ref_calculator.ref_data["xyz"],
-            s=self._ref_calculator.ref_data["s"],
-            q_core=self._ref_calculator.ref_data["q_core"],
-            q=self._ref_calculator.ref_data["q"],
-            alpha=self._ref_calculator.ref_data["alpha"],
+            z=ref_data["z"],
+            xyz=ref_data["xyz_qm"],
+            s=ref_data["s"],
+            q_core=ref_data["q_core"],
+            q_val=ref_data["q_val"],
+            alpha=ref_data["alpha"],
             train_mask=train_mask,
             alpha_mode=alpha_mode,
             sigma=sigma,
@@ -51,8 +98,12 @@ class EMLEBespoke:
             device=device,
             dtype=dtype,
         )
+        logger.info("Finished training bespoke model.")
 
-    def patch_model(self, model_filename="mymodel.mat"):
+    def patch_model(self):
+        """
+        Patch the model by finding optimal alpha and beta values.
+        """
         pass
 
     def _train(
@@ -61,27 +112,34 @@ class EMLEBespoke:
         xyz,
         s,
         q_core,
-        q,
+        q_val,
         alpha,
         train_mask=None,
         alpha_mode="species",
         sigma=1e-3,
         ivm_thr=0.02,
-        epochs=10000,
+        epochs=100,
         lr_qeq=0.01,
         lr_thole=0.01,
         lr_sqrtk=0.01,
-        model_filename="mymodel.mat",
+        model_filename="bespoke_model.mat",
         device=_torch.device("cuda"),
         dtype=_torch.float64,
     ):
+        """
+        Train a bespoke model.
+
+        Parameters
+        ----------
+        """
         trainer = _EMLETrainer(self._emle_base)
+
         trainer.train(
             z=z,
             xyz=xyz,
             s=s,
             q_core=q_core,
-            q=q,
+            q_val=q_val,
             alpha=alpha,
             train_mask=train_mask,
             alpha_mode=alpha_mode,
