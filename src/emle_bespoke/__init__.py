0ec295ed77a9b59f47ae617b1a13d41886e6af86
Joao Morado <joaomorado@gmail.com>
2024-11-06 17:54:17 +0000

    Rename ReferenceDataCalculator in init

--- src/emle_bespoke/__init__.py
+++ src/emle_bespoke/__init__.py
@@ -4,6 +4,7 @@ __version__ = "0.0.1"
 __author__ = "Joao Morado"
 
 from ._emle_bespoke import EMLEBespoke
-from ._sampler import ReferenceDataCalculator
+from ._sampler import ReferenceDataSampler
 
-__all__ = ["EMLEBespoke", "ReferenceDataCalculator"]
+__all__ = ["EMLEBespoke", "ReferenceDataSampler"]
+
