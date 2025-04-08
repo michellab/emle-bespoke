d0944976e4cbad3c091de85f3edd6ef3deb79fc5
Joao Morado <joaomorado@gmail.com>
2025-04-08 17:26:32 +0100

    Remove LJ fitting modules for now, preparing for the next update

--- src/emle_bespoke/lj_fitting/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Lennard-Jones fitting module."""
-
-from ._lj_potential import LennardJonesPotential
-from ._loss import InteractionEnergyLoss
-from ._loss_exp_reweighting import ReweightingLoss
-
-__all__ = ["LennardJonesPotential", "InteractionEnergyLoss", "ReweightingLoss"]
